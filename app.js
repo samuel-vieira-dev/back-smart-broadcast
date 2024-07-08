7f8eae30343420fa153fa204f338e200d41bfcc9
Samuel Vieira <samuelvieira.developer@gmail.com>
2024-07-08 15:43:05 -0300

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
